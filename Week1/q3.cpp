441f2a4f9e9252938027a6887058d3a0845d5101
anushkarajora11 <arajora961@gmail.com>
2025-05-31 16:27:16 +0530

    Add Week1 C++ files

---
+++ Week1/q3.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+using namespace std;
+#define maxTest 1000
+
+int main (){
+    int N;
+    cin>> N;
+    int A[maxTest] , B[maxTest];
+    for(int i=0;i<N;i++){
+    cin>> A[i];
+    }
+    
+    for(int i=0;i<N;i++){
+    cin>>B[i];
+    }
+    int total_beauty=0;
+    int current_position=0;
+    for(int i=0;i<N-1;i++){
+        for(int j=i+1;j<N;j++){
+         if(B[i]>B[j]){
+            int temp =B[i]; B[i]=B[j];B[j]=temp;
+            
+            temp =A[i]; A[i]=A[j];A[j]=temp;
+         }
+        }
+    }
+
+    for(int i=0;i<N;i++){
+     total_beauty += current_position*B[i];
+     current_position+= A[i];
+    }
+
+
+
+    cout<< total_beauty<<endl;
+     
+
+    return 0;
+}
---
+++ Week1/q5.cpp
@@ -0,0 +1,42 @@
+#include<iostream>
+#include <string>
+#include <stack>
+using namespace std;
+
+class Solution {
+public:
+    bool isBalanced(string s) {
+        stack<char> st;
+        for (char c : s) {
+            if (c == '(' || c == '{' || c == '[') {
+                st.push(c);
+            } else {
+                if (st.empty()) return false;
+
+                char top = st.top();
+                if ((c == ')' && top != '(') ||
+                    (c == '}' && top != '{') ||
+                    (c == ']' && top != '[')) {
+                    return false;
+                }
+                st.pop();
+            }
+        }
+        return st.empty();
+    }
+};
+int main(){
+    Solution q5;
+    char str[50];
+    fgets(str,50 , stdin);
+    puts(str);
+    if(q5.isBalanced(str)){
+        cout<<"false"<<endl;
+    }else{
+        cout<<"true"<<endl;
+    }
+
+
+
+  return 0;
+}
