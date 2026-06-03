42a497bfa61519a110bdfd79165d039a94e3dd9c
Anushka Rajora <arajora961@gmail.com>
2026-06-03 19:13:12 +0530

    Time: 2 ms (23.96%) | Memory: 11 MB (86.38%) - LeetSync

--- 1217-relative-sort-array/relative-sort-array.cpp
+++ 1217-relative-sort-array/relative-sort-array.cpp
@@ -2,28 +2,30 @@ class Solution {
 public:
     vector<int> relativeSortArray(vector<int>& arr1, vector<int>& arr2) {
 
-        vector<int> freq(1001, 0);
-
-        for(int x : arr1) {
-            freq[x]++;
-        }
-
         vector<int> ans;
+        vector<int> rem;
 
-        for(int x : arr2) {
+        for(int i = 0; i < arr2.size(); i++) {
 
-            while(freq[x] > 0) {
-                ans.push_back(x);
-                freq[x]--;
+            for(int j = 0; j < arr1.size(); j++) {
+
+                if(arr2[i] == arr1[j]) {
+                    ans.push_back(arr1[j]);
+                    arr1[j] = -1; // mark as used
+                }
             }
         }
 
-        for(int i = 0; i <= 1000; i++) {
-
-            while(freq[i] > 0) {
-                ans.push_back(i);
-                freq[i]--;
+        for(int i = 0; i < arr1.size(); i++) {
+            if(arr1[i] != -1) {
+                rem.push_back(arr1[i]);
             }
+        }
+
+        sort(rem.begin(), rem.end());
+
+        for(int x : rem) {
+            ans.push_back(x);
         }
 
         return ans;
