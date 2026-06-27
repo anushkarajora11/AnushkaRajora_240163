0c070a142fcb42ff6be68a3ca9867db8f4b9f3cc
Anushka Rajora <arajora961@gmail.com>
2026-06-27 19:33:47 +0530

    Time: 0 ms (100.00%) | Memory: 10.9 MB (42.68%) - LeetSync

--- 144-binary-tree-preorder-traversal/binary-tree-preorder-traversal.cpp
+++ 144-binary-tree-preorder-traversal/binary-tree-preorder-traversal.cpp
@@ -1,27 +1,50 @@
+// class Solution {
+// public:
+//     vector<int> preorderTraversal(TreeNode* root) {
+//         vector<int> ans;
+
+//         if (root == nullptr)
+//             return ans;
+
+//         stack<TreeNode*> st;
+//         st.push(root);
+
+//         while (!st.empty()) {
+//             TreeNode* node = st.top();
+//             st.pop();
+
+//             ans.push_back(node->val);
+
+//             if (node->right)
+//                 st.push(node->right);
+
+//             if (node->left)
+//                 st.push(node->left);
+//         }
+
+//         return ans;
+//     }
+// };
+
 class Solution {
 public:
     vector<int> preorderTraversal(TreeNode* root) {
         vector<int> ans;
-
-        if (root == nullptr)
-            return ans;
-
         stack<TreeNode*> st;
-        st.push(root);
+        TreeNode* curr = root;
 
-        while (!st.empty()) {
-            TreeNode* node = st.top();
+        while (curr || !st.empty()) {
+            while (curr) {
+                ans.push_back(curr->val);
+                st.push(curr);
+                curr = curr->left;
+            }
+            curr = st.top();
             st.pop();
-
-            ans.push_back(node->val);
-
-            if (node->right)
-                st.push(node->right);
-
-            if (node->left)
-                st.push(node->left);
+            
+            curr = curr->right;
         }
-
         return ans;
     }
 };
+
