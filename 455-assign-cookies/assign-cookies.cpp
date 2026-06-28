c7a97f87def0506be540bacd9316344cf49ee8a9
Anushka Rajora <arajora961@gmail.com>
2026-06-28 23:16:00 +0530

    Time: 3 ms (94.25%) | Memory: 44.8 MB (77.88%) - LeetSync

--- 455-assign-cookies/assign-cookies.cpp
+++ 455-assign-cookies/assign-cookies.cpp
@@ -9,11 +9,8 @@ public:
         while(i < g.size() && j < s.size()) {
             if(s[j] >= g[i]) {
                 i++;
-                j++;
             }
-            else {
-                j++;
-            }
+            j++;
         }
 
         return i;
