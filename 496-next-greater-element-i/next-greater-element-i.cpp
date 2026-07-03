153ef977ab48c3a4dd44a241730c27d9c3047cc1
Anushka Rajora <arajora961@gmail.com>
2026-07-04 03:08:51 +0530

    Time: 2 ms (46.89%) | Memory: 12.2 MB (92.62%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.cpp
+++ 496-next-greater-element-i/next-greater-element-i.cpp
@@ -3,18 +3,23 @@ public:
     vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
         vector<int> ans;
 
-        for(int x : nums1) {
-            int nge = -1;
-            bool found = false;
+        for(int i = 0; i < nums1.size(); i++) {
 
+            int x = nums1[i];
+            int nge = -1;
+
+            // find x in nums2
             for(int j = 0; j < nums2.size(); j++) {
 
                 if(nums2[j] == x) {
-                    found = true;
-                }
 
-                if(found && nums2[j] > x) {
-                    nge = nums2[j];
+                    // now check right side
+                    for(int k = j + 1; k < nums2.size(); k++) {
+                        if(nums2[k] > x) {
+                            nge = nums2[k];
+                            break;
+                        }
+                    }
                     break;
                 }
             }
