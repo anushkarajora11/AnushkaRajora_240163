020d54080f978c61c375e08f8e6444eb14c81239
Anushka Rajora <arajora961@gmail.com>
2026-07-08 01:34:55 +0530

    Time: 24 ms (78.07%) | Memory: 16.2 MB (80.34%) - LeetSync

--- 200-number-of-islands/number-of-islands.cpp
+++ 200-number-of-islands/number-of-islands.cpp
@@ -1,39 +1,33 @@
 class Solution {
 public:
+    void dfs(vector<vector<char>>& grid, int i, int j) {
+        int n = grid.size();
+        int m = grid[0].size();
+
+        // Base case
+        if (i < 0 || i >= n || j < 0 || j >= m || grid[i][j] == '0')
+            return;
+
+        // Mark as visited
+        grid[i][j] = '0';
+
+        // Visit all 4 directions
+        dfs(grid, i + 1, j);
+        dfs(grid, i - 1, j);
+        dfs(grid, i, j + 1);
+        dfs(grid, i, j - 1);
+    }
+
     int numIslands(vector<vector<char>>& grid) {
         int n = grid.size();
         int m = grid[0].size();
         int count = 0;
 
-        queue<pair<int, int>> q;
-        int dx[] = {1, -1, 0, 0};
-        int dy[] = {0, 0, 1, -1};
-
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
-
                 if (grid[i][j] == '1') {
                     count++;
-                    grid[i][j] = '0';
-                    q.push({i, j});
-
-                    while (!q.empty()) {
-                        auto [x, y] = q.front();
-                        q.pop();
-
-                        for (int k = 0; k < 4; k++) {
-                            int nx = x + dx[k];
-                            int ny = y + dy[k];
-
-                            if (nx >= 0 && nx < n &&
-                                ny >= 0 && ny < m &&
-                                grid[nx][ny] == '1') {
-
-                                grid[nx][ny] = '0';
-                                q.push({nx, ny});
-                            }
-                        }
-                    }
+                    dfs(grid, i, j);
                 }
             }
         }
