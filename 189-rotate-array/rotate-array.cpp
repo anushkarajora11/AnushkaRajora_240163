ca273248ef6580b645e9f60f5f1f104496aeca8d
Anushka Rajora <arajora961@gmail.com>
2026-07-08 23:13:56 +0530

    Time: 0 ms (100.00%) | Memory: 30.6 MB (12.35%) - LeetSync

--- 189-rotate-array/rotate-array.cpp
+++ 189-rotate-array/rotate-array.cpp
@@ -4,8 +4,12 @@ public:
         int n = nums.size();
         k %= n;
 
-        reverse(nums.begin(), nums.end());
-        reverse(nums.begin(), nums.begin() + k);
-        reverse(nums.begin() + k, nums.end());
+        vector<int> ans(n);
+
+        for(int i = 0; i < n; i++) {
+            ans[(i + k) % n] = nums[i];
+        }
+
+        nums = ans;
     }
 };
